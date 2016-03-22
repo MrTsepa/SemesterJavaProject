a01b8c0966fde9aacb696cb910f976a40d003180
Dashashant <shantdari@yandex.ru>
2016-03-22 14:27:49 +0300

    Update HelloJSFML.java

--- src/HelloJSFML.java
+++ src/HelloJSFML.java
@@ -1,3 +1,6 @@
+package client_socket;
+
+import java.io.IOException;
 import org.jsfml.graphics.*;
 import org.jsfml.system.Vector2f;
 import org.jsfml.window.Keyboard;
@@ -5,18 +8,22 @@ import org.jsfml.window.event.*;
 import org.jsfml.window.VideoMode;
 
 public class HelloJSFML {
-    public static void main(String args[]) {
+    socketConnection client1 = new socketConnection();
+        
+    public static void main(String args[]) throws IOException {
         RenderWindow window = new RenderWindow();
         window.create(new VideoMode(640, 480), "Hello JSFML!");
         window.setFramerateLimit(30);
+        socketConnection.Connect();
 
-        LocalSocketWrapper socketConnection = new LocalSocketWrapper();
+        //LocalSocketWrapper socketConnection = new LocalSocketWrapper();
 
-        CircleShape[] circleShapes = new CircleShape[socketConnection.getPlayerCount()];
+        CircleShape[] circleShapes = new CircleShape[2];
         circleShapes[0] = new CircleShape(50);
         circleShapes[1] = new CircleShape(50);
-        Vector2f[] XYArray;
-        Vector2f selfXY = socketConnection.getSelfXY();
+        //Vector2f[] XYArray;
+          Vector2f selfXY = new Vector2f(World.getSelfXY().x, World.getSelfXY().y);
+        
 
         circleShapes[0].setFillColor(Color.RED);
         circleShapes[1].setFillColor(Color.GREEN);
@@ -28,22 +35,24 @@ public class HelloJSFML {
                 }
                 if (event.type == Event.Type.KEY_PRESSED) {
                     if (event.asKeyEvent().key == Keyboard.Key.LEFT) {
-                        socketConnection.setSelfXY(new Vector2f(selfXY.x - 10, selfXY.y));
+                       World.setSelfXY(new Vector2f(selfXY.x - 10, selfXY.y));
                     }
                     if (event.asKeyEvent().key == Keyboard.Key.RIGHT) {
-                        socketConnection.setSelfXY(new Vector2f(selfXY.x + 10, selfXY.y));
+                         World.setSelfXY(new Vector2f(selfXY.x + 10, selfXY.y));
+                         System.out.println("hello!");
                     }
                     if (event.asKeyEvent().key == Keyboard.Key.UP) {
-                        socketConnection.setSelfXY(new Vector2f(selfXY.x, selfXY.y - 10));
+                         World.setSelfXY(new Vector2f(selfXY.x, selfXY.y - 10));
                     }
                     if (event.asKeyEvent().key == Keyboard.Key.DOWN) {
-                        socketConnection.setSelfXY(new Vector2f(selfXY.x, selfXY.y + 10));
+                         World.setSelfXY(new Vector2f(selfXY.x, selfXY.y + 10));
                     }
-                    selfXY = socketConnection.getSelfXY();
+                     selfXY = World.getSelfXY();
                 }
             }
 
-            XYArray = socketConnection.getRemoteXYArray();
+            Vector2f[] XYArray = new Vector2f[2];
+            XYArray = World.getRemoteXYArray();
             for (int i = 0; i < circleShapes.length; i++) {
                 circleShapes[i].setPosition(XYArray[i]);
             }
@@ -55,6 +64,6 @@ public class HelloJSFML {
 
             window.display();
         }
-    }
+   }
 }
 
