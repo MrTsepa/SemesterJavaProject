b4411e7b287e99a9ed1eaf5851b1625924f28bd3
TsepaStepan <tsepa.stepan@gmail.com>
2016-04-30 19:24:00 +0300

    добавил строчку package

--- src/server/Service.java
+++ src/server/Service.java
@@ -1,14 +1,9 @@
+package server;
+
 import java.io.BufferedInputStream;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.*;
 
+import world.*;
 
 public class Service {
     World ourWorld = new World();
