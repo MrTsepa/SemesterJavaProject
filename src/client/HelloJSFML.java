27f20805de2bf259a62e2e7458f1ee26f002fb77
TsepaStepan <tsepa.stepan@gmail.com>
2016-05-01 16:41:58 +0300

    drawThread

--- src/client/HelloJSFML.java
+++ src/client/HelloJSFML.java
@@ -19,7 +19,66 @@ public class HelloJSFML {
 
         @Override
         public void run() {
-            // TODO
+            RenderWindow window = new RenderWindow();
+            window.create(new VideoMode(640, 480), "Hello JSFML!");
+            window.setFramerateLimit(30);
+
+            ArrayList<Drawable> drawables = new ArrayList<>();
+
+            Cell cell1 = new Cell(new Vector2f(30, 10), 15, Team.Player1);
+            Cell cell2 = new Cell(new Vector2f(400, 200), 20, Team.Player1);
+            cell1.addTentacle(cell2);
+            drawables.add(cell1);
+            drawables.add(cell2);
+
+            float x = 0.0f;
+            Integer trianglePosition = 0;
+            boolean flag = true;
+
+            while (window.isOpen()) {
+                for (Event event : window.pollEvents()) {
+                    if (event.type == Event.Type.CLOSED) {
+                        window.close();
+                    }
+                    if (event.type == Event.Type.KEY_PRESSED) {
+                        if (event.asKeyEvent().key == Keyboard.Key.LEFT) {
+                        }
+                        if (event.asKeyEvent().key == Keyboard.Key.RIGHT) {
+                        }
+                        if (event.asKeyEvent().key == Keyboard.Key.UP) {
+                        }
+                        if (event.asKeyEvent().key == Keyboard.Key.DOWN) {
+                        }
+                    }
+                }
+
+                cell1.tentacleSet.iterator().next().setDistancePart(x);
+                if(x < 1)
+                    x += 0.006f;
+                else {
+                    cell1.tentacleSet.iterator().next().yellowTriangles.remove(trianglePosition);
+                    if (trianglePosition == 0)
+                        trianglePosition = cell1.tentacleSet.iterator().next().getTriangleCount();
+                    else {
+                        if (flag) {
+                            trianglePosition--;
+                            flag = false;
+                        } else {
+                            flag = true;
+                        }
+                        // магия
+                    }
+                    cell1.tentacleSet.iterator().next().yellowTriangles.add(trianglePosition);
+                }
+                window.clear(Color.BLACK);
+
+                for (Drawable drawable :
+                        drawables) {
+                    window.draw(drawable);
+                }
+
+                window.display();
+            }
         }
     }
 
@@ -40,65 +99,8 @@ public class HelloJSFML {
     }
 
     public static void main(String args[]) throws IOException {
-        RenderWindow window = new RenderWindow();
-        window.create(new VideoMode(640, 480), "Hello JSFML!");
-        window.setFramerateLimit(30);
-
-        ArrayList<Drawable> drawables = new ArrayList<>();
-
-        Cell cell1 = new Cell(new Vector2f(30, 10), 15, Team.Player1);
-        Cell cell2 = new Cell(new Vector2f(400, 200), 20, Team.Player1);
-        cell1.addTentacle(cell2);
-        drawables.add(cell1);
-        drawables.add(cell2);
-
-        float x = 0.0f;
-        Integer trianglePosition = 0;
-        boolean flag = true;
-        while (window.isOpen()) {
-            for (Event event : window.pollEvents()) {
-                if (event.type == Event.Type.CLOSED) {
-                    window.close();
-                }
-                if (event.type == Event.Type.KEY_PRESSED) {
-                    if (event.asKeyEvent().key == Keyboard.Key.LEFT) {
-                    }
-                    if (event.asKeyEvent().key == Keyboard.Key.RIGHT) {
-                    }
-                    if (event.asKeyEvent().key == Keyboard.Key.UP) {
-                    }
-                    if (event.asKeyEvent().key == Keyboard.Key.DOWN) {
-                    }
-                }
-            }
-
-            cell1.tentacleSet.iterator().next().setDistancePart(x);
-            if(x < 1)
-                x += 0.006f;
-            else {
-                cell1.tentacleSet.iterator().next().yellowTriangles.remove(trianglePosition);
-                if (trianglePosition == 0)
-                    trianglePosition = cell1.tentacleSet.iterator().next().getTriangleCount();
-                else {
-                    if (flag) {
-                        trianglePosition--;
-                        flag = false;
-                    } else {
-                        flag = true;
-                    }
-                     // магия
-                }
-                cell1.tentacleSet.iterator().next().yellowTriangles.add(trianglePosition);
-            }
-            window.clear(Color.BLACK);
-
-            for (Drawable drawable :
-                    drawables) {
-                window.draw(drawable);
-            }
-
-            window.display();
-        }
-   }
+        Thread drawThread = new Thread(new DrawRunnable());
+        drawThread.start();
+    }
 }
 
