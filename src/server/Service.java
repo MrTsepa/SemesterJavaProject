80d2647b0066484c1218c844f1c4b24cab576bab
Dashashant <shantdari@yandex.ru>
2016-05-20 15:25:50 +0300

    Update Service.java

--- src/server/Service.java
+++ src/server/Service.java
@@ -8,8 +8,20 @@ import org.jsfml.system.Vector2f;
 import world.World;
 import world.game.objects.Cell;
 import world.game.Team;
+import org.jsfml.system.Vector2i;
+import org.jsfml.window.VideoMode;
+import org.jsfml.window.event.Event;
 
-public class Service {
+import utils.Geometry;
+import world.UpdateRunnable;
+import world.World;
+import world.game.Team;
+import world.game.objects.Cell;
+import world.game.events.*;
+import world.game.objects.Tentacle;
+import static utils.Geometry.findIntersectionPart;
+
+public class Service{
  
     Cell cell1 = new Cell(new Vector2f(100, 100), 20, Team.Player1);
     Cell cell2 = new Cell(new Vector2f(200, 100), 20, Team.Player1);
@@ -21,23 +33,26 @@ public class Service {
     world.World world = new world.World(cell1, cell2, cell3, cell4, cell5, cell6);
     
     public int id = 0;
-    public int nextId() {return id++;}
-    
-    public void serve(InputStream i, OutputStream o) throws IOException{
+    public synchronized int nextId() {
+        return id++;
+    }
+   
+    public void sendWorld(InputStream i, OutputStream o) throws IOException{
         System.out.println("in serve");
-        // настраиваем потоки
-        
-        //ObjectInputStream inObject = new ObjectInputStream(i);
         System.out.println("Objectstream in created " );
-        
         ObjectOutputStream outObject = new ObjectOutputStream(o);
-        
-        //записываем новый мир и отправляем его
-        //ourWorld = (World) inObject;
         System.out.println("Ready to serve" );
+        world.playerNumber = nextId();
         outObject.writeObject(world);
         System.out.println("world sent");
-       //s outObject.writeObject(nextId());
+    }
+    public void readEvent(InputStream i, OutputStream o) throws IOException, ClassNotFoundException{
+        ObjectInputStream inObject = new ObjectInputStream(i);
+        ObjectOutputStream outObject = new ObjectOutputStream(o);
+        while(true){
+            Event event = (Event) inObject.readObject();
+            outObject.writeObject(event);
+        }    
         
     }
     
