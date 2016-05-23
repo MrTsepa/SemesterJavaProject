7819e4b1d8b522a12f1779a3af50731b92607f83
Dashashant <shantdari@yandex.ru>
2016-05-23 20:40:05 +0300

    Update Service.java

--- src/server/Service.java
+++ src/server/Service.java
@@ -1,43 +1,16 @@
-package server;
-
-
-import java.io.BufferedInputStream;
 
 import java.io.*;
-import org.jsfml.system.Vector2f;
 import world.World;
-import world.game.objects.Cell;
-import world.game.Team;
-import org.jsfml.system.Vector2i;
-import org.jsfml.window.VideoMode;
-import org.jsfml.window.event.Event;
+import world.game.events.Event;
 
-import utils.Geometry;
-import world.UpdateRunnable;
-import world.World;
-import world.game.Team;
-import world.game.objects.Cell;
-import world.game.events.*;
-import world.game.objects.Tentacle;
-import static utils.Geometry.findIntersectionPart;
 
 public class Service{
- 
-    Cell cell1 = new Cell(new Vector2f(100, 100), 20, Team.Player1);
-    Cell cell2 = new Cell(new Vector2f(200, 100), 20, Team.Player1);
-    Cell cell3 = new Cell(new Vector2f(100, 200), 20, Team.Player2);
-    Cell cell4 = new Cell(new Vector2f(300, 200), 20, Team.Player2);
-    Cell cell5 = new Cell(new Vector2f(150, 150), 10, Team.Neutral);
-    Cell cell6 = new Cell(new Vector2f(400, 400), 20, Team.Player1);
-
-    world.World world = new world.World(cell1, cell2, cell3, cell4, cell5, cell6);
-    
     public int id = 0;
     public synchronized int nextId() {
         return id++;
     }
-
-    public void sendWorld(OutputStream o) throws IOException{
+   
+    public void sendWorld(OutputStream o, World world) throws IOException{
         System.out.println("in serve");
         System.out.println("Objectstream in created " );
         ObjectOutputStream outObject = new ObjectOutputStream(o);
@@ -46,11 +19,12 @@ public class Service{
         outObject.writeObject(world);
         System.out.println("world sent");
     }
-    public void readEvent(InputStream i) throws IOException, ClassNotFoundException{
+    public Event readEvent(InputStream i) throws IOException, ClassNotFoundException{
         ObjectInputStream inObject = new ObjectInputStream(i);
-        while(true){
-            Event event = (Event) inObject.readObject();
-        }
+        Event event = (Event) inObject.readObject();
+        System.out.println("Read event in readEvent");
+        return event;
     }
+    
 }
 
